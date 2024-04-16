be2d0ff1a9cefadf4ec403ba49d1e5f92232804f
Erick Valencia <evalencia0911@gmail.com>
2024-04-15 18:51:31 -0600

    wip: play around with pieces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+    import Header from '../assests/Header.svelte'
+    import Footer from '../assests/Footer.svelte'
+</script>
+
+<Header />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Footer />
