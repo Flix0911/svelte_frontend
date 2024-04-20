f2e26788744dc7d89a1f34beb3ec1daaf3673edd
Erick Valencia <evalencia0911@gmail.com>
2024-04-20 10:05:44 -0600

    wip: add images and good memories

--- src/routes/home/images/add/+page.svelte
+++ src/routes/home/images/add/+page.svelte
@@ -2,11 +2,39 @@
     import Header from '../../../../assests/Header.svelte'
     import Footer from '../../../../assests/Footer.svelte'
     import Navigation from '../../../../assests/Navigation.svelte'
+    import { ImagesStore } from '../../../../stores/images-store';
+    import { goto } from '$app/navigation';
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    // state 
+    let files;
+
+    // submit function
+    let handleSubmit = () => {
+        const call =  `${apiUrl}images/`
+        console.log(call)
+        let data = new FormData()
+        console.log(data)
+        data.append('image', files[0])
+    
+    fetch(call, {method: 'POST', body: data}).then(response => response.json()).then(data => {
+        ImagesStore.update(prev => [...prev, data])
+        goto('/home/images/')
+    })
+}
+
+
 </script>
 
 <Header />
 <Navigation />
 
+<div>
+    <form on:submit|preventDefault={handleSubmit}>
+        <input class="dummy-class" type="file" bind:files/>
+        <button class="submit" type="submit">Submit</button>
+    </form>
+</div>
 <h2>This is routes/home/images/add/page</h2>
 
 
